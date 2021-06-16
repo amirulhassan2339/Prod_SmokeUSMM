c1c7122ef83006427da7f36e080b92965492d648
Hafiz Amir Ul Hassan <Hafiz.Amir@soliton.local>
2021-06-16 13:06:57 -0700

    Chagne for test on Jenkins

--- ReportsFolder/report.js
+++ ReportsFolder/report.js
@@ -1,8 +1,56 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Include/features/PatientGrid/SoapNote/SoapNotesCreate_AddHRA.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Include/features/PatientGrid/SearchPatient.feature");
 formatter.feature({
-  "name": "Soap Note Creation from Schedule flow",
+  "name": "Search Patient",
   "description": "",
-  "keyword": "Feature"
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@PatientGrid"
+    }
+  ]
+});
+formatter.scenarioOutline({
+  "name": "Search Patient Using MRN From Patient Grid",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@SearchByMRN"
+    }
+  ]
+});
+formatter.step({
+  "name": "I search \u003cPatient\u003e using global search",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "I should see \u003cPatient_Name\u003e as patient with \u003cMRN\u003e as MRN and \u003cDOB\u003e as DOB and \u003cPatient_Status\u003e as Status on patient grid",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "Patient",
+        "Patient_Name",
+        "MRN",
+        "DOB",
+        "Patient_Status"
+      ]
+    },
+    {
+      "cells": [
+        "PE05E6ECEF0E46443B887BA108B24D9049",
+        "ADam, Tom",
+        "PE05E6ECEF0E46443B887BA108B24D9049",
+        "02/02/2000",
+        "Active"
+      ]
+    }
+  ]
 });
 formatter.background({
   "name": "",
@@ -23,61 +71,34 @@ formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Verify deleting Scheduled Appointment",
+  "name": "Search Patient Using MRN From Patient Grid",
   "description": "",
-  "keyword": "Scenario",
+  "keyword": "Scenario Outline",
   "tags": [
     {
-      "name": "@Smoke_USMM_DelateScheduleAppointment"
+      "name": "@PatientGrid"
+    },
+    {
+      "name": "@SearchByMRN"
     }
   ]
 });
 formatter.step({
-  "name": "I click on schedule tab",
+  "name": "I search PE05E6ECEF0E46443B887BA108B24D9049 using global search",
   "keyword": "When "
 });
 formatter.match({
-  "location": "SD_SoapNotes.I_click_on_scheduletab()"
+  "location": "SD_SearchPatient.search_Patient(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "I should see already scheduled appointment",
+  "name": "I should see ADam, Tom as patient with PE05E6ECEF0E46443B887BA108B24D9049 as MRN and 02/02/2000 as DOB and Active as Status on patient grid",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SD_SoapNotes.I_should_see_Already_Added_Appointment()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on three dots",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "SD_SoapNotes.I_Click_Three_Dots()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on delete appointment",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "SD_SoapNotes.I_Click_delete_appointmentBtn()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I should see delete appointment message",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "SD_SoapNotes.I_should_see_appointment_DeleteMessage()"
+  "location": "SD_SearchPatient.Patient_should_be_verified(String,String,String,String)"
 });
 formatter.result({
   "status": "passed"
